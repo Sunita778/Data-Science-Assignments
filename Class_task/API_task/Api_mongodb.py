37e66827a603e4dc259258851e4771586acf350e
Sunita Pradhan <103746954+Sunita778@users.noreply.github.com>
2022-08-24 10:55:49 +0530

    Add files via upload

---
+++ Class_task/API_task/Api_mongodb.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, jsonify
+import pymongo
+
+app = Flask(__name__)
+
+client = pymongo.MongoClient("mongodb+srv://Sunita7:Sunita778@cluster0.t5kpe6z.mongodb.net/?retryWrites=true&w=majority")
+
+database = client['task_mongo']
+collection = database['task_collection']
+
+@app.route("/insert/mongo", methods = ['POST'])
+def insert():
+    if request.method == 'POST':
+        name = request.json['name']
+        number = request.json['number']
+        collection.insert_one({name : number})
+        return jsonify(str("successfully inserted"))
+
+@app.route("/update/mongo", methods = ['POST'])
+def update():
+    if request.method == 'POST':
+        name = request.json['name']
+        number = request.json['number']
+        collection.update_one({name: number})
+        return jsonify(str("successfully updated"))
+
+@app.route("/delete/mongo", methods = ['POST'])
+def delete():
+    if request.method == 'POST':
+        name = request.json['name']
+        number = request.json['number']
+        collection.delete_one({name: number})
+        return jsonify(str("successfully deleted"))
+
+@app.route("/fetch/mongo", methods = ['POST'])
+def fetch():
+    if request.method == 'POST':
+        name = request.json['name']
+        number = request.json['number']
+        for i in collection.find():
+            return jsonify(str(i))
+
+if __name__ == '__main__':
+    app.run()   # or for change port number app.run(port = 5001)
---
+++ Class_task/API_task/Api_sql.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify
+import mysql.connector as conn
+
+app = Flask(__name__)
+
+mydb = conn.connect(host= 'localhost',user= 'root', passwd= 'Sunil@78645')
+cursor = mydb.cursor()
+cursor.execute("create database if not exists task_sql")
+cursor.execute("create table if not exists task_sql.mytable(name varchar(30), number int)")
+
+@app.route('/insert', methods= ['POST'])
+def insert():
+    if (request.method == 'POST'):
+        name = request.json['name']
+        number = request.json['number']
+        cursor.execute("insert into task_sql.mytable values(%s, %s)", (name, number))
+        mydb.commit()
+        return jsonify(str("successfully inserted"))
+
+@app.route("/update", methods= ['POST'])
+def update():
+    if request.method == 'POST':
+        get_name = request.json['get_name']
+        cursor.execute("update task_sql.mytable set number = number + 500 where name = %s", (get_name,))
+        mydb.commit()
+        return jsonify(str("updated successfully"))
+
+@app.route("/delete", methods= ['POST'])
+def delete():
+    if (request.method == 'POST'):
+        name_del =request.json['name_del']
+        cursor.execute("delete from task_sql.mytable where name = %s", (name_del,))
+        mydb.commit()
+        return jsonify(str("deleted successfully"))
+
+@app.route("/fetch", methods= ['POST'])
+def fetch():
+    if (request.method == 'POST'):
+        cursor.execute("select * from task_sql.mytable")
+        for i in cursor.fetchall():
+            return jsonify(str(i))
+
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+
